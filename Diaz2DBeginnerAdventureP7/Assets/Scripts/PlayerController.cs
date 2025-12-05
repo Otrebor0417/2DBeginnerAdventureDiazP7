f05453c51216efc9a3114c2bec1dbc25ac20a468
Otrebor0417 <145072312+Otrebor0417@users.noreply.github.com>
2025-12-05 15:25:22 -0800

    jh,sdvfuyvf
    fsdfsf

--- Diaz2DBeginnerAdventureP7/Assets/Scripts/PlayerController.cs
+++ Diaz2DBeginnerAdventureP7/Assets/Scripts/PlayerController.cs
@@ -3,26 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class PlayerController : MonoBehaviour
 {
+    // Variables related to player character movement
     public InputAction MoveAction;
-    public float speed = 6.0f;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
+    public float speed = 3.0f;
+
+
+    // Variables related to the health system
+    public int maxHealth = 5;
+    int currentHealth;
+
 
     // Start is called before the first frame update
     void Start()
-    { 
-        
-        //  LeftAction.Enable();
+    {
         MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
+        move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 0.1f * Time.deltaTime;
-       
-        transform.position = position;
     }
+
+
+    // FixedUpdate has the same call rate as the physics system
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
 }
